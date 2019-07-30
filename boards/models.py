6b946106fa2bd48558aacd5b18ae6ca162a0ea86
mansnotsauce <35285189+mansnotsauce@users.noreply.github.com>
2019-07-30 13:23:57 -0600

    Update models.py

--- boards/models.py
+++ boards/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Result(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
+    img = models.ImageField()
     sop_instance_uid = models.CharField(max_length=64)
     tests = models.CharField(max_length=2)
     results = models.TextField()
